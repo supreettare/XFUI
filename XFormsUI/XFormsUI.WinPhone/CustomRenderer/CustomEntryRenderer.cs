c4e6b29b19528e0307f5697176b0f17533118211
Supreet <tare.supreet@gmail.com>
2015-07-02 14:35:46 +0530

    WP complete

---
+++ XFormsUI/XFormsUI.WinPhone/CustomRenderer/CustomEntryRenderer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Phone.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WinPhone;
+using XFormsUI.WinPhone.CustomRenderer;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+
+namespace XFormsUI.WinPhone.CustomRenderer
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                return;
+            }
+
+            var nativePhoneTextBox = (PhoneTextBox)this.Control.Children[0];
+            nativePhoneTextBox.Background = new SolidColorBrush(Colors.Transparent);
+            nativePhoneTextBox.BorderThickness = new System.Windows.Thickness(0);
+
+            nativePhoneTextBox.HintStyle = (System.Windows.Style)App.Current.Resources["LoginHintTextBlockStyle"];
+                      
+        }
+    }
+}
--- XFormsUI/XFormsUI/Classes/DoubleResources.cs
+++ XFormsUI/XFormsUI/Classes/DoubleResources.cs
@@ -10,6 +10,8 @@ namespace XFormsUI.Classes
     public static class DoubleResources
     {
         public static readonly Thickness ButtonGroupPadding = new Thickness(0, Device.OnPlatform(25, 0, 0), 0, 0);
+        public static readonly double SignUpButtonHeight = Device.OnPlatform(40, 40, 80);
+        public static readonly double FBButtonHeight = Device.OnPlatform(50, 50, 64);
     }
 }
     
